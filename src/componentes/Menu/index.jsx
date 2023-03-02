f072831c0bd5d144c6ccba017e807d677c1a7e35
Eduardo Raiol <marquesraiol@gmail.com>
2023-03-02 14:54:20 -0300

    Criação do componente Banner

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -4,33 +4,33 @@ import maisCurtidas from '../../assets/icones/mais-curtidas.png'
 import maisVistas from '../../assets/icones/mais-vistas.png'
 import novas from '../../assets/icones/novas.png'
 import surpreendaMe from '../../assets/icones/surpreenda-me.png'
-import styles from './Menu.module.scss'
+import styles from './Menu.module.scss';
 
 export default function Menu() {
   return (
     <nav className={styles.menu}>
-        <ul className={styles.menu__lista}>
-            <li className={styles.menu__item}>
-                <img src={home} alt="" />
-                <a href="/" alt="">Início </a>
-            </li> 
-            <li className={styles.menu__item}>
-                <img src={maisCurtidas} alt="" />
-                <a href="/" alt="">Mais curtidas </a>
-            </li> 
-            <li className={styles.menu__item}>
-                <img src={maisVistas} alt="" />
-                <a href="/" alt="">Mais vistas </a>
-            </li> 
-            <li className={styles.menu__item}>
-                <img src={novas} alt="" />
-                <a href="/" alt="">Novas </a>
-            </li> 
-            <li className={styles.menu__item}>
-                <img src={surpreendaMe} alt="" />
-                <a href="/" alt="">Surpreenda-me </a>
-            </li>    
-        </ul>    
+      <ul className={styles.menu__lista}>
+        <li className={styles.menu__item}>
+          <img src={home} alt="" />
+          <a href="/">Início</a>
+        </li>
+        <li className={styles.menu__item}>
+          <img src={maisCurtidas} alt="" />
+          <a href="/">Mais curtidas</a>
+        </li>
+        <li className={styles.menu__item}>
+          <img src={maisVistas} alt="" />
+          <a href="/">Mais vistas</a>
+        </li>
+        <li className={styles.menu__item}>
+          <img src={novas} alt="" />
+          <a href="/">Novas</a>
+        </li>
+        <li className={styles.menu__item}>
+          <img src={surpreendaMe} alt="" />
+          <a href="/">Surpreenda-me</a>
+        </li>
+      </ul>
     </nav>
   )
 }
--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,19 +1,21 @@
+import banner from "./banner.png";
 import Cabecalho from "../../componentes/Cabecalho";
 import Menu from "../../componentes/Menu";
-import Banner from "../../componentes/Banner";
-import styles from './PaginaInicial.module.scss';
+import styles from "./PaginaInicial.module.scss";
 
 export default function PaginaInicial() {
-    return (
-        <>
-            <Cabecalho />
-            <main>
-                <section className={styles.principal}>
-                    <Menu />
-                </section>
-                <Banner />
-                
-            </main>
-        </>
-    )
+  return (
+    <>
+      <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={banner} alt="A imagem da terra vista do espaço" />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
